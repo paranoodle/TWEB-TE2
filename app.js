1e170b1b312de61b35fb19cad10ec44cdc1156ba
paranoodle <paranoodle@users.noreply.github.com>
2016-01-21 21:08:09 +0100

    does this still work?

--- app.js
+++ app.js
@@ -68,4 +68,10 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+//module.exports = app;
+
+app.set('port', process.env.PORT || 3000);
+
+var server = http.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
